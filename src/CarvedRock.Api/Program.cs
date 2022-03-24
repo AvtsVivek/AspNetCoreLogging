50286615cf51d0b04b81dde53327407004f4ef6a
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-03-24 18:41:35 +0530

    Added database code project

--- src/CarvedRock.Api/Program.cs
+++ src/CarvedRock.Api/Program.cs
@@ -1,9 +1,16 @@
+using System.Diagnostics;
+using CarvedRock.Data;
 using CarvedRock.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
+//builder.Logging.AddFilter("CarvedRock", LogLevel.Debug);
 
-// Add services to the container.
+// var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+// var tracePath = Path.Join(path, $"Log_CarvedRock_{DateTime.Now.ToString("yyyyMMdd-HHmm")}.txt");        
+// Trace.Listeners.Add(new TextWriterTraceListener(System.IO.File.CreateText(tracePath)));
+// Trace.AutoFlush = true;	
 
+// Services
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -11,19 +18,27 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProductLogic, ProductLogic>();
 
+builder.Services.AddDbContext<LocalContext>();
+builder.Services.AddScoped<ICarvedRockRepository, CarvedRockRepository>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<LocalContext>();
+    context.MigrateAndCreateData();
+}
+
+// HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MapFallback(() => Results.Redirect("/swagger"));
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
